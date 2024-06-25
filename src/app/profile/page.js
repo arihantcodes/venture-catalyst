fbb00bf912d8056d7bc0cc193f8406b7eaa47d21
itzamanjain <aman.044063@tmu.ac.in>
2024-06-25 22:23:15 +0530

    added profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,5 +1,11 @@
 const Page = () => {
-    return <div className="text-white">Profile Page</div>;
+    return(
+        <>
+            <h1 className="text-white text-center text-2xl " >Profile Page</h1>
+            <p className="text-white text-center text-2xl ">under construction !! </p>
+        
+        </>
+    );
 }
 
 export default Page
