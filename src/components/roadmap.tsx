5158bca238df9e39eb0b6f5a30bdc95b0255631f
arihant-jain0 <jainari1208@gmail.com>
2024-06-25 19:17:24 +0530

    add:image C capital

--- src/components/roadmap.tsx
+++ src/components/roadmap.tsx
@@ -28,7 +28,7 @@ const Roadmap = () => {
           height={400}
         />
         <div className="md:hidden block ">
-          <Image src="/content.svg" alt="Content" width={200} height={200} />
+          <Image src="/Content.svg" alt="Content" width={200} height={200} />
         </div>
       </div>
     </div>
