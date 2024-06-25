9e13c7f752676ea14318089e761172295f121923
arihant-jain0 <jainari1208@gmail.com>
2024-06-25 20:45:28 +0530

    fix: Update navbar link to point to signup page

--- src/components/magicui/display/navbar.tsx
+++ src/components/magicui/display/navbar.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="hidden md:block">
-          <Link href="/">
+          <Link href="/signup">
             <button className="bg-black text-white px-4 py-2 rounded-full hover:bg-gray-800">
               Get Started
             </button>
