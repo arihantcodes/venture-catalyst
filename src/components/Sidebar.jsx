4dbb3f9c1b30401a5b69f8410eafe89578ea968e
itzamanjain <aman.044063@tmu.ac.in>
2024-07-04 12:11:06 +0530

    chore: Refactor Sidebar component and update sidebar links

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -58,49 +58,56 @@ const Sidebar =() => {
 
   return (
     <div>
-      {/* Hamburger Menu Icon */}
-      <div className="md:hidden flex justify-end p-4 bg-black">
-        <button onClick={toggleMenu} className="text-white focus:outline-none">
-          <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M4 6h16M4 12h16m-7 6h7"></path>
-          </svg>
-        </button>
-      </div>
-      {/* Sidebar Content */}
-      <div className={`min-h-screen w-[15rem] border-r border-gray-700 mr-10 bg-black text-white p-4 transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 fixed md:relative z-10`}>
-        <div className="flex flex-col items-center ml-8 md:block">
-          <Image src="/Vector.svg" alt="Profile Picture" width={96} height={96} className="rounded-full mb-10" />
-          {/* <Link href="/" passHref>
-            <span className="text-xl font-bold mb-4 cursor-pointer">VCatalyst</span>
-          </Link> */}
-          <nav className="space-y-4">
-            <Link href="/roadmap" passHref>
-              <span className="flex text-xl items-center space-x-2 hover:bg-gray-700 p-2 rounded-md cursor-pointer">Road-Map</span>
-            </Link>
-            <Link href="/module" passHref>
-              <span className="flex text-xl items-center space-x-2 hover:bg-gray-700 p-2 rounded-md cursor-pointer">Module</span>
-            </Link>
-            <Link href="/explore" passHref>
-              <span className="flex text-xl items-center space-x-2 hover:bg-gray-700 p-2 rounded-md cursor-pointer">Explore</span>
-            </Link>
-            <Link href="/share" passHref>
-              <span className="flex text-xl items-center space-x-2 hover:bg-gray-700 p-2 rounded-md cursor-pointer">Share</span>
-            </Link>
-          </nav>
-        </div>
-        
-        <div className="absolute bottom-0 left-0 flex flex-col right-0 p-4">
-            <button 
-            onClick={handleLogout}
-            className='bg-red-600 text-lg p-2 m-3 text-white rounded-lg ' >
-              Logout
-            </button>
-            <span className="text-center text-sm">© 2024 VCatalyst</span>
-            <span className="text-center text-sm">V 0.0.2</span>
-        </div>
-      </div>
-      <Toaster />
+    {/* Hamburger Menu Icon */}
+    <div className="md:hidden flex justify-end p-4 bg-black">
+      <button onClick={toggleMenu} className="text-white focus:outline-none">
+        <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+          <path d="M4 6h16M4 12h16m-7 6h7"></path>
+        </svg>
+      </button>
     </div>
+    {/* Sidebar Content */}
+    <div className={`min-h-screen w-[15rem] border-r border-gray-700 mr-10 bg-black text-white p-4 transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 fixed md:relative z-10`}>
+      <div className="flex flex-col items-center ml-8 md:block">
+        <Image src="/Vector.svg" alt="Profile Picture" width={96} height={96} className="rounded-full mb-10" />
+        <nav className="space-y-4">
+          <Link href="/roadmap" passHref>
+            <span className="flex text-xl items-center space-x-2 hover:bg-gray-700 p-2 rounded-md cursor-pointer">Road-Map</span>
+          </Link>
+          <Link href="/module" passHref>
+            <span className="flex text-xl items-center space-x-2 hover:bg-gray-700 p-2 rounded-md cursor-pointer">Module</span>
+          </Link>
+          <Link href="/explore" passHref>
+            <span className="flex text-xl items-center space-x-2 hover:bg-gray-700 p-2 rounded-md cursor-pointer">Explore</span>
+          </Link>
+          <Link href="/share" passHref>
+            <span className="flex text-xl items-center space-x-2 hover:bg-gray-700 p-2 rounded-md cursor-pointer">Share</span>
+          </Link>
+        </nav>
+      </div>
+  
+      <div className="flex-grow overflow-y-auto">
+        <div className="flex flex-col items-center ml-8 md:block">
+          {/* Add your sidebar links and other content here */}
+
+          
+
+        </div>
+      </div>
+  
+      <div className="absolute bottom-0 left-0 flex flex-col right-0 p-4">
+        <button 
+          onClick={handleLogout}
+          className='bg-red-600 text-lg p-2 m-3 text-white rounded-lg'>
+          Logout
+        </button>
+        <span className="text-center text-sm">© 2024 VCatalyst</span>
+        <span className="text-center text-sm">V 0.0.2</span>
+      </div>
+    </div>
+    <Toaster />
+  </div>
+  
   );
 }
 
