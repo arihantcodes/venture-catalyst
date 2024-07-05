dfff45c2242f8c5d2f6b60c9349b877407dec19b
arihant-jain0 <jainari1208@gmail.com>
2024-07-05 18:51:38 +0530

    add: profile routes

--- src/app/api/v1/auth/profile/route.ts
+++ src/app/api/v1/auth/profile/route.ts
@@ -7,17 +7,12 @@ import streamifier from 'streamifier';
 
 // Configure cloudinary
 cloudinary.config({
-<<<<<<< HEAD
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true,
-=======
+
  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
  api_key: process.env.CLOUDINARY_API_KEY,
  api_secret: process.env.CLOUDINARY_API_SECRET,
  secure: true,
->>>>>>> f7884781d6d1ca60ae1f9c67f62d9ab10e74f04d
+
 });
 
 // Connect to the database
@@ -25,19 +20,6 @@ connectDb();
 
 // Upload image to cloudinary
 const uploadAvatar = (buffer: Buffer) => {
-<<<<<<< HEAD
-  return new Promise((resolve, reject) => {
-    const uploadStream = cloudinary.uploader.upload_stream((error, result) => {
-      if (result) {
-        resolve(result.secure_url);
-      } else {
-        reject(error);
-      }
-    });
-
-    streamifier.createReadStream(buffer).pipe(uploadStream);
-  });
-=======
  return new Promise((resolve, reject) => {
  const uploadStream = cloudinary.uploader.upload_stream((error, result) => {
  if (result) {
@@ -49,73 +31,12 @@ const uploadAvatar = (buffer: Buffer) => {
 
  streamifier.createReadStream(buffer).pipe(uploadStream);
  });
->>>>>>> f7884781d6d1ca60ae1f9c67f62d9ab10e74f04d
+
 };
 
 // Create a new profile for a user, taking their bio, profile pic, venture name, and LinkedIn username
 export async function POST(request: NextRequest) {
-<<<<<<< HEAD
-  try {
-    console.log('Received POST request');
 
-    const data = await request.formData();
-    console.log('Request body:', data);
-
-    if (!data) {
-      return NextResponse.json({ message: 'Please provide required fields' }, { status: 400 });
-    }
-
-    const bio = data.get('bio');
-    const ventureName = data.get('ventureName');
-    const linkedinUrl = data.get('linkedinUrl');
-    const profilePic = data.get('profilePic') as File;
-
-    console.log('Fields:', { bio, ventureName, linkedinUrl, profilePic });
-
-    if (!bio || !ventureName || !linkedinUrl || !profilePic) {
-      console.log('Missing fields:', { bio, ventureName, linkedinUrl, profilePic });
-      return NextResponse.json({ message: 'Please fill in all fields' }, { status: 400 });
-    }
-
-    // Take token from cookie and get the user ID from it to assign userId to the profile
-    const userId = getDataFromToken(request);
-    console.log('User ID from token:', userId);
-
-    const existingProfile = await Profile.findOne({ userId });
-    if (existingProfile) {
-      console.log('Profile already exists for user ID:', userId);
-      return NextResponse.json({ message: 'Profile already exists' }, { status: 400 });
-    }
-
-    // Convert profilePic to buffer
-    const buffer = Buffer.from(await profilePic.arrayBuffer());
-
-    // Upload profile picture and create a new profile
-    const uploadPicUrl = await uploadAvatar(buffer);
-    console.log('Uploaded profile picture URL:', uploadPicUrl);
-
-    const newProfile = new Profile({
-      bio,
-      ventureName,
-      linkedinUrl,
-      userId,
-      profilePictureUrl: uploadPicUrl,
-    });
-
-    const savedProfile = await newProfile.save();
-    console.log('Profile created:', savedProfile);
-
-    return NextResponse.json({
-      message: 'Profile created successfully',
-      success: true,
-      savedProfile,
-    });
-  } catch (error: any) {
-    console.error('Error:', error.message);
-    return NextResponse.json({ message: 'Failed to create profile' }, { status: 500 });
-  }
-}
-=======
  try {
  console.log('Received POST request');
 
@@ -176,4 +97,4 @@ export async function POST(request: NextRequest) {
  return NextResponse.json({ message: 'Failed to create profile' }, { status: 500 });
  }
 }
->>>>>>> f7884781d6d1ca60ae1f9c67f62d9ab10e74f04d
+
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,6 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 
-<<<<<<< HEAD
-=======
-  //return NextResponse.redirect(new URL('/dashboard', request.url))
->>>>>>> f7884781d6d1ca60ae1f9c67f62d9ab10e74f04d
 
 export const config = {
   matcher: [
