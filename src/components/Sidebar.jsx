f3b321da10515858289a77d69d4d6069f3bc8fd4
itzamanjain <aman.044063@tmu.ac.in>
2024-07-05 11:49:11 +0530

    chore: Refactor Sidebar component, update sidebar links, and improve code readability

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,17 +4,18 @@ import Image from 'next/image';
 import Link from 'next/link';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
-import toast ,{Toaster} from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
+import { Button } from "@/components/ui/button";
 
-const Sidebar =() => {
+const Sidebar = () => {
 
-  const router = useRouter(); 
+  const router = useRouter();
 
   const [isOpen, setIsOpen] = useState(false);
 
   const handleLogout = async () => {
     try {
-      
+
       const response = await axios.get('/api/v1/auth/logout');
       toast.success('Logged out successfully');
       if (response.status === 200) {
@@ -58,61 +59,79 @@ const Sidebar =() => {
 
   return (
     <div>
-    {/* Hamburger Menu Icon */}
-    <div className="md:hidden flex justify-end p-4 bg-black">
-      <button onClick={toggleMenu} className="text-white focus:outline-none">
-        <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="M4 6h16M4 12h16m-7 6h7"></path>
-        </svg>
-      </button>
-    </div>
-    {/* Sidebar Content */}
-    <div className={`min-h-screen w-[15rem] border-r border-gray-700 mr-10 bg-black text-white p-4 transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 fixed md:relative z-10`}>
-      <div className="flex flex-col items-center ml-8 md:block">
-        <Image src="/Vector.svg" alt="Profile Picture" width={96} height={96} className="rounded-full mb-10" />
-        <nav className="space-y-4">
-          <Link href="/roadmap" passHref>
-            <span className="flex text-xl items-center space-x-2 hover:bg-gray-700 p-2 rounded-md cursor-pointer">Road-Map</span>
-          </Link>
-          <Link href="/module" passHref>
-            <span className="flex text-xl items-center space-x-2 hover:bg-gray-700 p-2 rounded-md cursor-pointer">Module</span>
-          </Link>
-          <Link href="/explore" passHref>
-            <span className="flex text-xl items-center space-x-2 hover:bg-gray-700 p-2 rounded-md cursor-pointer">Explore</span>
-          </Link>
-          <Link href="/share" passHref>
-            <span className="flex text-xl items-center space-x-2 hover:bg-gray-700 p-2 rounded-md cursor-pointer">Share</span>
-          </Link>
-          <button
-          onClick={handleLogout}
-          className='block md:hidden bg-red-500 hover:bg-red-700 rounded-lg text-white ' >
-            <span className="flex text-xl items-center space-x-2 p-2 rounded-md cursor-pointer">Logout</span>
-          </button>
-        </nav>
+      {/* Hamburger Menu Icon */}
+      <div className="md:hidden flex justify-end p-4 bg-black">
+        <button onClick={toggleMenu} className="text-white focus:outline-none">
+          <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M4 6h16M4 12h16m-7 6h7"></path>
+          </svg>
+        </button>
       </div>
-  
-      <div className="flex-grow overflow-y-auto">
+      {/* Sidebar Content */}
+      <div className={`min-h-screen w-[15rem] border-r border-gray-700 mr-10 bg-black text-white p-4 transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 fixed md:relative z-10`}>
         <div className="flex flex-col items-center ml-8 md:block">
-          {/* Add your sidebar links and other content here */}
+          <Image src="/Vector.svg" alt="Profile Picture" width={96} height={96} className="rounded-full mb-10" />
+          <nav className="space-y-4">
+            <Link className='flex items-center space-x-1 hover:bg-gray-700 p-1 rounded-md cursor-pointer' href="/roadmap" passHref>
 
-          
+              <Image
+                src='/rmicon.png'
+                width={24}
+                height={24}
+              />
 
+              <span className=" text-xl items-center  hover:bg-gray-700  rounded-md cursor-pointer">Roadmap</span>
+            </Link>
+            {/* <Link href="/module" passHref>
+              <span className="flex text-xl items-center space-x-2 hover:bg-gray-700 p-2 rounded-md cursor-pointer">Module</span>
+            </Link> */}
+            <Link className='flex items-center space-x-1 hover:bg-gray-700 p-2 rounded-md cursor-pointer' href="/explore" passHref>
+              <Image
+                width={24}
+                height={24}
+                src='/pficon.png'
+              />
+              <span className="flex text-xl items-center  hover:bg-gray-700  rounded-md cursor-pointer">Explore</span>
+            </Link>
+            <Link className='flex items-center space-x-1 hover:bg-gray-700 p-2 rounded-md cursor-pointer' href="/share" passHref>
+              <Image
+                width={20}
+                height={20}
+                src='/shareicon.png'
+              />
+              <span className="flex text-xl items-center  hover:bg-gray-700 rounded-md cursor-pointer">Share</span>
+            </Link>
+            <Button
+              variant={'outline'}
+              onClick={handleLogout}
+              className='block md:hidden bg-red-500 hover:bg-red-700 rounded-lg text-white ' >
+              Logout
+            </Button>
+          </nav>
+        </div>
+
+        <div className="flex-grow overflow-y-auto">
+          <div className="flex flex-col items-center ml-8 md:block">
+            {/* Add your sidebar links and other content here */}
+
+
+
+          </div>
+        </div>
+
+        <div className="absolute bottom-0 left-0 flex flex-col right-0 p-4">
+          <button
+            onClick={handleLogout}
+            className=' hidden md:block  bg-red-600 text-lg p-2 m-3 text-white rounded-lg'>
+            Logout
+          </button>
+          <span className="text-center text-sm">© 2024 VCatalyst</span>
+          <span className="text-center text-sm">V 0.0.2</span>
         </div>
       </div>
-  
-      <div className="absolute bottom-0 left-0 flex flex-col right-0 p-4">
-        <button 
-          onClick={handleLogout}
-          className=' hidden md:block  bg-red-600 text-lg p-2 m-3 text-white rounded-lg'>
-          Logout
-        </button>
-        <span className="text-center text-sm">© 2024 VCatalyst</span>
-        <span className="text-center text-sm">V 0.0.2</span>
-      </div>
+      <Toaster />
     </div>
-    <Toaster />
-  </div>
-  
+
   );
 }
 
--- src/components/magicui/display/dash.tsx
+++ src/components/magicui/display/dash.tsx
@@ -61,8 +61,7 @@ const GridBackgroundDemo = () => {
             onClick={handleClick}
             variant={"outline"}
             className=" w-full py-3 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-white font-bold rounded-lg focus:outline-none"
-            
-            
+ 
           >
             Join The Community
           </Button>
